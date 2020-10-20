414b1b8b980b2cf1dd54ee0ebf18dfe0eb1250d4
Birajit <birajit95@gmail.com>
2020-10-20 11:19:50 +0530

    Add: flipCoin() function is added to flip the coin

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -1,3 +1,14 @@
 #! /bin/bash -x
 
 echo "Welcome to Flip coin simulation program"
+
+flipCoin(){
+    
+    if [ $(( RANDOM%2 )) -eq 1 ]
+    then
+        echo "Head"
+    else
+        echo "Tail"
+    fi        
+}
+flipCoin
