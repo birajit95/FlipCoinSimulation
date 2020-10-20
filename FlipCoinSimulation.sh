cf0a34f89bcc526295b9555b21dce6d5b058ff05
Birajit <birajit95@gmail.com>
2020-10-20 11:47:06 +0530

    Add: flippingGame() is added to flip coin multiple times and to show the result

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -11,4 +11,32 @@ flipCoin(){
         echo "Tail"
     fi        
 }
-flipCoin
+
+
+flippingGame(){
+
+    local headCount=0
+    local tailCount=0
+
+    count=1
+    while [ $count -le 100 ]
+    do
+      if [ $(flipCoin) == 'Head' ]
+      then
+          ((headCount++))
+      else
+          ((tailCount++))
+      fi   
+
+      ((count++))
+
+    done
+
+    echo "Head has won $headCount times"
+
+    echo "Tail has won $tailCount times"
+
+   
+}
+
+flippingGame
